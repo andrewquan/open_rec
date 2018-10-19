4273771681cabe15dcf48f6bcdec5d58fbccabb7
Andrew Quan <andrew.h.quan@gmail.com>
2018-10-19 23:17:09 +0900

    add start_time and end_time to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012171158) do
+ActiveRecord::Schema.define(version: 20181019125545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20181012171158) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "group_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "groups", force: :cascade do |t|
