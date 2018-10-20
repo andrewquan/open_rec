f3721d4b06905d7476241f29c91c4361f0501ea9
Andrew Quan <andrew.h.quan@gmail.com>
2018-10-20 20:02:55 +0900

    some more view updates

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,7 +1,12 @@
 module RegistrationsHelper
   def minimum_password_hint
     if @minimum_password_length
-      "; #{@minimum_password_length} characters minimum"
+      # need to make this more dynamic
+      if signed_in?
+        "; #{@minimum_password_length} characters minimum"
+      else
+        "#{@minimum_password_length} characters minimum"
+      end
     end
   end
 end
